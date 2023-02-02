13d437371fd00f927bcdab91cf2ffea965441b54
Arif Hidayat <rizentium@gmail.com>
2023-02-02 20:39:33 +0700

    feat(screen): add inspector screen

--- lib/lithium_http_inspector.dart
+++ lib/lithium_http_inspector.dart
@@ -3,5 +3,6 @@ library lithium_http_inspector;
 import './src/lithium_http_inspector_platform.dart';
 
 export './lithium_http_inspector.dart';
+export './src/screen/http_inspector_screen.dart';
 
 part './src/lithium_http_inspector.dart';
---
+++ lib/src/screen/http_inspector_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HttpInspectorScreen extends StatelessWidget {
+  const HttpInspectorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lithium Inspector'),
+      ),
+    );
+  }
+}
